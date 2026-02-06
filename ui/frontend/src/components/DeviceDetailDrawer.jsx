02bd87abc7607a530e64d2b5b5819e434e9b0e74
gonglijing <gong@kdisi.com>
2026-02-07 02:16:50 +0800

    chore(ui): update device detail drawer

--- ui/frontend/src/components/DeviceDetailDrawer.jsx
+++ ui/frontend/src/components/DeviceDetailDrawer.jsx
@@ -112,6 +112,7 @@ function DeviceDetailDrawer(props) {
                               <td>{formatDateTime(a.triggered_at)}</td>
                             </tr>
                           )}
+                        </For>
                       </tbody>
                     </table>
                   </div>
